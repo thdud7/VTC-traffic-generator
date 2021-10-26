7702d0a9cce112bf3c5beec1491c935a37ad13ee
Dave DeAngelis <deangelis@isi.edu>
2021-10-25 19:32:30 -0700

    Draft merge model complete.

--- testbed/vtc_dcomp_model.py
+++ testbed/vtc_dcomp_model.py
@@ -35,18 +35,18 @@ def node(name, node_type, group):
 vtc_hosts = [node(name, "rohu", 1) for name in ['jitsi', 'vtc_controller', 'client_0', 'client_1', 'client_2']]
 
 # Connect hosts to each other
-net.connect(vtc_hosts, capacity == mbps(100))
+net.connect((vtc_hosts[0], vtc_hosts[2], vtc_hosts[3], vtc_hosts[4]), capacity == mbps(100))
+net.connect((vtc_hosts[1], vtc_hosts[2], vtc_hosts[3], vtc_hosts[4]), capacity == mbps(100))
 
 # IP addresses
 vtc_hosts[0].endpoints[0].ip.addrs = ['10.0.0.100/24']  # jitsi             == 10.0.0.100
-vtc_hosts[1].endpoints[0].ip.addrs = ['10.0.0.101/24']  # vtc_controller    == 10.0.0.101
+vtc_hosts[1].endpoints[0].ip.addrs = ['10.0.1.101/24']  # vtc_controller    == 10.0.1.101
 vtc_hosts[2].endpoints[0].ip.addrs = ['10.0.0.102/24']  # vtc_client0_trfc  == 10.0.0.102
-#vtc_hosts[2].endpoints[1].ip.addrs = ['10.0.1.102/24']  # vtc_client0_ctrl  == 10.0.1.102
+vtc_hosts[2].endpoints[1].ip.addrs = ['10.0.1.102/24']  # vtc_client0_ctrl  == 10.0.1.102
 vtc_hosts[3].endpoints[0].ip.addrs = ['10.0.0.103/24']  # vtc_client1_trfc  == 10.0.0.103
-#vtc_hosts[3].endpoints[1].ip.addrs = ['10.0.1.103/24']  # vtc_client1_ctrl  == 10.0.1.103
+vtc_hosts[3].endpoints[1].ip.addrs = ['10.0.1.103/24']  # vtc_client1_ctrl  == 10.0.1.103
 vtc_hosts[4].endpoints[0].ip.addrs = ['10.0.0.104/24']  # vtc_client2_trfc  == 10.0.0.104
-#vtc_hosts[4].endpoints[1].ip.addrs = ['10.0.1.104/24']  # vtc_client2_ctrl  == 10.0.1.104
+vtc_hosts[4].endpoints[1].ip.addrs = ['10.0.1.104/24']  # vtc_client2_ctrl  == 10.0.1.104
 
 # start the experiment
 mx.experiment(net)
-
