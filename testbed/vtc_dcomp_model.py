f78e8010d7f976c0d1a0d8bed439c87446bdb9d0
Dave DeAngelis <deangelis@isi.edu>
2021-10-25 19:13:55 -0700

    Compiles, need to break up into 2 lans

--- testbed/vtc_dcomp_model.py
+++ testbed/vtc_dcomp_model.py
@@ -1,7 +1,7 @@
 import mergexp as mx
-from mergexp.net import capacity, routing, static
-from mergexp.unit import mbps, gb
-from mergexp.machine import cores, memory
+from mergexp.unit import gb, mbps
+from mergexp.machine import memory, image
+from mergexp.net import capacity, latency, routing, static, addressing, ipv4
 
 # create a topology
 net = mx.Topology('vtc', routing == static, addressing == ipv4)
@@ -41,13 +41,11 @@ net.connect(vtc_hosts, capacity == mbps(100))
 vtc_hosts[0].endpoints[0].ip.addrs = ['10.0.0.100/24']  # jitsi             == 10.0.0.100
 vtc_hosts[1].endpoints[0].ip.addrs = ['10.0.0.101/24']  # vtc_controller    == 10.0.0.101
 vtc_hosts[2].endpoints[0].ip.addrs = ['10.0.0.102/24']  # vtc_client0_trfc  == 10.0.0.102
-vtc_hosts[2].endpoints[1].ip.addrs = ['10.0.1.102/24']  # vtc_client0_ctrl  == 10.0.1.102
+#vtc_hosts[2].endpoints[1].ip.addrs = ['10.0.1.102/24']  # vtc_client0_ctrl  == 10.0.1.102
 vtc_hosts[3].endpoints[0].ip.addrs = ['10.0.0.103/24']  # vtc_client1_trfc  == 10.0.0.103
-vtc_hosts[3].endpoints[1].ip.addrs = ['10.0.1.103/24']  # vtc_client1_ctrl  == 10.0.1.103
+#vtc_hosts[3].endpoints[1].ip.addrs = ['10.0.1.103/24']  # vtc_client1_ctrl  == 10.0.1.103
 vtc_hosts[4].endpoints[0].ip.addrs = ['10.0.0.104/24']  # vtc_client2_trfc  == 10.0.0.104
-vtc_hosts[4].endpoints[1].ip.addrs = ['10.0.1.104/24']  # vtc_client2_ctrl  == 10.0.1.104
-
-alpha[1].endpoints[0].ip.addrs = ['10.0.0.101/24']  # h1 == 10.0.0.101
+#vtc_hosts[4].endpoints[1].ip.addrs = ['10.0.1.104/24']  # vtc_client2_ctrl  == 10.0.1.104
 
 # start the experiment
 mx.experiment(net)
